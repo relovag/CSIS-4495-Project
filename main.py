7c280a01a93d4869f8e00cf0c4a00ca67b9a51b1
relovag <relovag@student.douglascollege.ca>
2021-07-01 09:29:25 -0700

    Add EDA section to the application

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import constants
 import model_builder
+import explore_data
 
 
 def main():
@@ -9,11 +10,17 @@ def main():
     st.title("Automated Machine Learning CSIS 4495")
     st.subheader("Zito Relova 300306471")
 
+    activities = ["EDA", "Model Building"]
     file = st.file_uploader("Upload Dataset", type=["csv", "txt"])
+    choice = st.sidebar.selectbox("Select Activity", activities, 1)
 
     if file:
-        builder = model_builder.ModelBuilder(file)
-        builder.run_model_builder()
+        if choice == "Model Building":
+            builder = model_builder.ModelBuilder(file)
+            builder.run_model_builder()
+        elif choice == "EDA":
+            eda = explore_data.EDA(file)
+            eda.run_eda()
 
 
 if __name__ == "__main__":
